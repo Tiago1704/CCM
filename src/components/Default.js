abbe81eabff2ed3ad1b18c7d8a4f2f62b0eb7a14
Tiago1704 <36915137+Tiago1704@users.noreply.github.com>
2020-08-09 22:50:21 -0300

    Add files via upload

---
+++ src/components/Default.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class Default extends Component {
+  render() {
+    console.log(this.props);
+    return(
+      <div className = "container">
+        <div className = "row">
+          <div className="col-10 mx-auto text-center text-title text-uppercase pt-5">
+            <h1 className = "display-3">No disponible en esta versión</h1>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
---
+++ src/data.js
@@ -0,0 +1,116 @@
+/*
+https://www.iconfinder.com/icons/1243689/call_phone_icon
+Creative Commons (Attribution 3.0 Unported);
+https://www.iconfinder.com/Makoto_msk */
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Bidon térmico",
+    img: "img/BidonTermico3L.png",
+    price: 665,
+    company: "Bazar",
+    info:
+      "Bidón térmico que mantendrá todo frío en tus viajes",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Kit termomate serigrafeado blanco",
+    img: "img/KitTermomateSerigrafeadoBlanco3.png",
+    price: 652,
+    company: "Termomate",
+    info:
+      "Un lindo kit de termomate serigrafeado re cheto, encima blanco",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "Termo de acero con diseño",
+    img: "img/TermoDeAceroConDiseño.png",
+    price: 8,
+    company: "Termomate",
+    info:
+      "Osea, es un termo... De acero... Con diseño! compralo, bana",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "Termo deportivo liso",
+    img: "img/TermoDeportivoLiso.png",
+    price: 1010,
+    company: "Termomate",
+    info:
+      "La cuarentena te pegó mal? querés fantasmearla haciendo deporte? entonces comprá esto, papá.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "Termo deportivo metalico (Marcas)",
+    img: "img/TermoDeportivoMetalicoMarcas.png",
+    price: 1043,
+    company: "Termomate",
+    info:
+      "Tenés cara de gastar plata en algo que podés conseguir más barato, pero tiene marcas! vale la pena, no?",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Termo Heinzel de metal",
+    img: "img/TermoHeinzelMetal.png",
+    price: 1258,
+    company: "Termomate",
+    info:
+      "Osea, boló: No es un termo cualquiera... Es un termo Heinzel! encima de metal jajaja re cheto.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "Termomate economico",
+    img: "img/TermomateEconomico.png",
+    price: 430,
+    company: "Termomate",
+    info:
+      "Te pegó la realidad de la cuarentena? pero aún así querés un buen termomate? comprá, papi. Comprá.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Termomate Liso Pastel",
+    img: "img/TermomateLisoPastel.png",
+    price: 638,
+    company: "Termomate",
+    info:
+      "Desde las oficinas de 'sos re maraca' llega: el termomate liso pastel! para demostrar como te comés el pedazo.",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 8,
+  title: "Termomate Liso Pastel",
+  img: "img/TermomateLisoPastel.png",
+  price: 638,
+  company: "Termomate",
+  info:
+    "Desde las oficinas de 'sos re maraca' llega: el termomate liso pastel! para demostrar como te comés el pedazo.",
+  inCart: false,
+  count: 0,
+  total: 0
+};
